e63db2540f97296f86b01655c809b298869c956c
disclose-tech <152188727+disclose-tech@users.noreply.github.com>
2026-07-08 17:39:56 +0200

    Date corrections

--- scraper/date_corrections.py
+++ scraper/date_corrections.py
@@ -1,3 +1,21 @@
 DATE_CORRECTIONS = {
-    # "identifiant": "2021-03-15",
+    # "file_id": "YYYY-MM-DD",
+    "ckKxG0ha9SFc5ppMoEAaaejJapnwwp8Q": "2012-12-12", # not imported yet
+    "buDY4O7ZgFQwe6XzkaEnQl2QJRJTG4oo": "2026-01-13", # not imported yet
+    "8aa100b17ce5c679017ce604596c001c": "2018-08-20", # corrected
+    "8a8d1b97617a17c001617b3186e1000a": "2000-04-11", # corrected
+    "8ac510ca59fdb10c0159fe38af7e00b2": "2017-01-30", # corrected
+    "8ab90ca24ce63a70014ce6b6f619022e": "2014-03-20", # corrected
+    "8ab90ca26307317e0163073a22550041": "1973-05-23", # corrected
+    "8acb340158fc74db0158fcb91db5005a": "2016-11-28", # corrected
+    "8ac510ca59fdb10c0159fe355805008d": "2017-01-30", # corrected
+    "8ab90ca255e3619e0155e3684a760004": "2015-01-09", # corrected
+    "8ab90ca254287de501542880ba310004": "2016-03-16", # corrected 
+    "8abb00ab51f7a7630151f7b48ce50005": "2015-12-18", # corrected
+    "8a8d1b9759cbba8a0159cbd1e1d5000c": "2016-11-21", # corrected
+    "8a9fb09d79f4a6b50179f5b6f238009f": "2021-04-15", # corrected
+    "8aac032479c76f3f0179c7e588030006": "2021-05-27", # corrected
+    "8ac510ca74fdfe860174fe53f762002a": "2020-09-29", # corrected
+    "8aac03246f9e3b5e016f9fb4d6280691": "2019-06-03", # corrected
+    "8aa100b156fe846a0156fea30825000b": "1996-08-08", # not imported yet
 }
